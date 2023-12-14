cb8005ef04e763e3522c04a3748eda34d0566ee8
javico0604 <javicosta060402@gmail.com>
2023-12-14 13:23:47 +0100

    Practica 1

---
+++ src/app/articulo/articulo.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Articulo } from '../Modelos/articulo';
+import { ARTICULOS } from '../Modelos/articulos';
+
+@Component({
+  selector: 'app-articulo',
+  templateUrl: './articulo.component.html',
+  styleUrls: ['./articulo.component.css']
+})
+export class ArticuloComponent {
+ articulos:Array<Articulo> = ARTICULOS 
+articulo:Articulo=this.articulos[3]
+botonActivo:boolean=true
+
+
+
+constructor(){
+  console.log("Constructor...")
+}
+ngOnInit(){
+  console.log("ngOnInit...")
+  setTimeout(() => {alert("Activo boton...");
+                    this.botonActivo=false},5000);
+}
+
+}
+
+
